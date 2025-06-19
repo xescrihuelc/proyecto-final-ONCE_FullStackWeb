7897d3e7cdd37cdc11edda5038a7a6e1b560842a
Joaquim Escrihuela <xescrihuelc@outlook.com>
2025-06-19 10:27:15 +0200

    corrective commit for backend

--- backend/index.js
+++ backend/index.js
@@ -3,26 +3,20 @@ const PORT = 8080;
 const express = require("express");
 const cors = require("cors");
 const { dbConnection } = require("./db");
-const { auth } = require("./middlewares/auth");
+// const { auth } = require("./middlewares/auth");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger-output.json");
 
-// const env = require("./config")
-
-const recipesRoutes = require("./routes/recipes.routes");
-const ingredientsRoutes = require("./routes/ingredients.routes");
-const usersRoutes = require("./routes/users.routes");
+const templateRoutes = require("./routes/template.routes");
 
 const main = () => {
     const app = express();
     app.use(cors());
     app.use(express.json());
-    // console.log("OpenAI: ", process.env.OPENAI_KEY);
-    // console.log("OpenAI: ", process.env.OPENAI_KEY);
+    console.log("Mongo URL: ", process.env.MONGO_URI);
 
-    app.use("/recipes", auth, recipesRoutes);
-    app.use("/ingredients", auth, ingredientsRoutes);
-    app.use("/", usersRoutes);
+    // app.use("/ingredients", auth, templateRoutes);
+    app.use("/", templateRoutes);
     app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     dbConnection();
