480530a902c02cee0a6fe6be5f63a4b894f40097
Diego Cantalapiedra <source@cantalaweb.com>
2025-06-20 03:31:15 +0200

    Added users and tasks routes.

---
+++ backend/routes/tasks.routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const tasksController = require("../controllers/tasks.controller");
+
+router.get("/", tasksController.getAllTasks);
+router.get("/:id", tasksController.getTaskById);
+router.post("/", tasksController.createTask);
+router.patch("/:id", tasksController.updateTask);
+
+module.exports = router;
