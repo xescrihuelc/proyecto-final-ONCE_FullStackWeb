75ac5f4be36d01264ad11cd931ee4069f1b81f3d
Diego Cantalapiedra <diego.cantalapiedra@sistemasgenomicos.com>
2025-06-20 20:40:51 +0200

    Added delete functions to the controllers and routes.

--- backend/routes/tasks.routes.js
+++ backend/routes/tasks.routes.js
@@ -6,5 +6,6 @@ router.get("/", tasksController.getAllTasks);
 router.get("/:id", tasksController.getTaskById);
 router.post("/", tasksController.createTask);
 router.patch("/:id", tasksController.updateTask);
+router.delete("/:id", tasksController.deleteTask);
 
 module.exports = router;
