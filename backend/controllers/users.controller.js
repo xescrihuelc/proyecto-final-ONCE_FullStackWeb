a522cb12a8ec5049ad75ae0e943f1c0d773342dc
Joaquim Escrihuela <xescrihuelc@outlook.com>
2025-06-20 11:59:15 +0200

    added comprobations and important fields on /users POST endpoint

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,8 +1,8 @@
+const { mongoose } = require("mongoose");
 const { Users } = require("../models/users.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
-const { default: mongoose } = require("mongoose");
 
 const login = async (req, res) => {
     //
@@ -28,14 +28,57 @@ const login = async (req, res) => {
 };
 
 const createUser = async (req, res) => {
-    // Recibir email, password y name
-    const { email, password, name } = req.body;
+    // Recibir email, password, name, surnames, roles, dailyHours, assignedTasks
+    const {
+        email,
+        password,
+        name,
+        surnames,
+        roles,
+        dailyHours,
+        assignedTasks,
+    } = req.body;
     if (!email || !password) {
         return res.status(404).json({ error: "Missing email or password" });
     }
-    if (!name) {
-        return res.status(404).json({ error: "Missing required name" });
+
+    if (!name || !surnames) {
+        return res
+            .status(404)
+            .json({ error: "Missing required name or surnames" });
     }
+
+    if (!roles) {
+        return res.status(404).json({ error: "Missing required roles" });
+    } else {
+        if (!Array.isArray(roles)) {
+            return res.status(406).json({ error: "Roles must be an array" });
+        }
+        if (roles.length === 0) {
+            return res
+                .status(406)
+                .json({ error: "Roles array cannot be empty" });
+        }
+    }
+
+    if (!dailyHours) {
+        return res.status(404).json({ error: "Daily hours required" });
+    } else {
+        if (typeof dailyHours !== "number" || dailyHours < 0) {
+            return res.status(406).json({
+                error: "Daily hours must be a number and not negative",
+            });
+        }
+    }
+
+    if (!assignedTasks) {
+        return res.status(404).json({ error: "Required tasks array" });
+    } else {
+        if (!Array.isArray(assignedTasks)) {
+            return res.status(406).json({ error: "Roles must be an array" });
+        }
+    }
+
     // Hashear password
     const hashedPassword = bcrypt.hashSync(password);
     // Añadir usuario en la DB
@@ -43,8 +86,11 @@ const createUser = async (req, res) => {
         const user = new Users({
             _id: new mongoose.Types.ObjectId(),
             name: name,
-            email: email,
+            surnames: surnames,
             password: hashedPassword,
+            email: email,
+            roles: roles,
+            dailyHours: dailyHours,
         });
         await user.save();
         res.status(201).send("User created");
