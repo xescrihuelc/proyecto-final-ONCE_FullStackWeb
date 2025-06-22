0c44fe39b0a2c31460710bee890f3ea87fca7e6a
Joaquim Escrihuela <xescrihuelc@outlook.com>
2025-06-23 00:27:13 +0200

    Added abstractor checkImportantField() function

--- backend/controllers/sesame.controller.js
+++ backend/controllers/sesame.controller.js
@@ -1,11 +1,31 @@
 const mongoose = require("mongoose");
 const { Sesame } = require("../models/sesame.model");
-// const jwt = require("jsonwebtoken");
-// const { JWT_SECRET } = require("../config");
+
+const checkImportantField = async (employeeIds, from, to) => {
+    if (!employeeIds) {
+        const err_msg = "No 'employeeIds' (ObjectId) field recieved";
+        return [false, err_msg];
+    }
+    if (!from) {
+        const err_msg = "No 'from' field (Date YYYY/MM/DD) recieved";
+        return [false, err_msg];
+    }
+    if (!to) {
+        const err_msg = "No 'to' field (Date YYYY/MM/DD) recieved";
+        return [false, err_msg];
+    }
+    return [true];
+};
 
 const getWorkedDays = async (req, res) => {
   try {
     const { employeeIds, from, to, limit = 10, page = 1 } = req.body;
+
+    const areImportantFieldsPresent = await checkImportantField(employeeIds, from, to);
+
+    if (areImportantFieldsPresent[0] == false) {
+        return res.status(406).json({ error: `${areImportantFieldsPresent[1]}` });
+    }
 
     const skip = (page - 1) * limit;
     const paginatedIds = employeeIds.slice(skip, skip + limit);
