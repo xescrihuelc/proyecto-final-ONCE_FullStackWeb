23a4798c87adcd41a835f32ba4228de42a7c8eba
Diego Cantalapiedra <diego.cantalapiedra@sistemasgenomicos.com>
2025-07-15 10:55:19 +0200

    Added Sesame fetching of employee data to the User management page.

--- backend/models/sesame.model.js
+++ backend/models/sesame.model.js
@@ -2,32 +2,27 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const fichajeSchema = new mongoose.Schema({
-    date: { type: Date, required: true },
-    secondsWorked: { type: Number, required: true },
+  date: { type: Date, required: true },
+  secondsWorked: { type: Number, required: true },
 });
 
 const sesameSchema = new mongoose.Schema({
-    _id: Schema.Types.ObjectId,
-    employeeId: { type: String, required: true },
-    days: {
-        type: [fichajeSchema],
-        default: [],
-    },
+  _id: Schema.Types.ObjectId,
+  employeeId: { type: String, required: true },
+  days: {
+    type: [fichajeSchema],
+    default: [],
+  },
 });
 
 const sesameUserSchema = new mongoose.Schema({
-    _id: Schema.Types.ObjectId,
-    employeeId: { type: String, required: true },
-    firstName: String,
-    lastName: String,
-    email: String,
-    workStatus: String,
-    imageProfileURL: String,
-    phone: String,
-    dateOfBirth: Date,
-    status: String,
-    professionalCategoryDescription: String,
-    jobChargeName: String
+  _id: Schema.Types.ObjectId,
+  employeeId: { type: String, required: true },
+  firstName: String,
+  lastName: String,
+  email: String,
+  imageProfileURL: String,
+  jobChargeName: String,
 });
 
 const Sesame = mongoose.model("Sesame", sesameSchema);
