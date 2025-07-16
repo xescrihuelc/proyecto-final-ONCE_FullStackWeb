a0fa436912d9a26643ec9c53359337c3cc3f3761
Diego Cantalapiedra <diego.cantalapiedra@sistemasgenomicos.com>
2025-07-16 02:33:32 +0200

    Changed PATCH request to PUT; give a new look to the user cards; and maje it work.

--- backend/routes/sesame.routes.js
+++ backend/routes/sesame.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const sesameController = require("../controllers/sesame.controller");
 
-router.get("/worked-absence-days", sesameController.getWorkedDays);
+router.get("/employees", sesameController.getSesameUser);
 router.post("/worked-absence-days", sesameController.getWorkedDays);
 
 module.exports = router;
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -6,7 +6,7 @@ router.post("/login", usersController.loginUser);
 router.get("/", usersController.getAllUsers);
 router.get("/:id", usersController.getUserById);
 router.post("/", usersController.createUser);
-router.patch("/:id", usersController.updateUser);
+router.put("/:id", usersController.replaceUser);
 router.delete("/:id", usersController.deleteUser);
 
 module.exports = router;
